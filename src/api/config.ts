c5b9dd30956d22203a50b1e4fdb666e674767046
Janet <jane824691@gmail.com>
2026-02-03 10:34:31 +0800

    feat: addProducts api

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,5 @@
 export const API_SERVER = 'http://localhost:3002'
 
-export const PRODUCT = API_SERVER + '/product/api'
+export const PRODUCTS = API_SERVER + '/product/api'
+export const PRODUCTS_ADD = API_SERVER + '/product/add'
 
--- src/api/product.ts
+++ src/api/product.ts
@@ -1,6 +1,6 @@
-import { PRODUCT } from './config'
+import { PRODUCTS, PRODUCTS_ADD } from './config'
 
-interface ProductParams {
+interface SearchProductParams {
     page?: number
     searchWord?: string
     priceLow?: number
@@ -10,7 +10,26 @@ interface ProductParams {
     lang?: string
 }
 
-export const getProducts = async (params: ProductParams = {}) => {
+interface ProductFile {
+    productImg?: File
+    photoContentMain?: File
+    photoContentSecondary?: File
+    photoContent?: File
+}
+
+interface AddProductParams {
+    categoryId?: number,
+    productName?: string,
+    productPrice?: number,
+    stock?: number,
+    productDescription?: string,
+    productNameEn?: string,
+    productDescriptionEn?: string,
+    files?: ProductFile,
+    lang?: string
+}
+
+export const getProducts = async (params: SearchProductParams = {}) => {
     const {
         page = 1,
         searchWord = '',
@@ -31,7 +50,7 @@ export const getProducts = async (params: ProductParams = {}) => {
     })
 
     const response = await fetch(
-        PRODUCT + `?${queryParams.toString()}`,
+        PRODUCTS + `?${queryParams.toString()}`,
         {
             headers: {
                 'Accept-Language': lang,
@@ -44,3 +63,54 @@ export const getProducts = async (params: ProductParams = {}) => {
     return data
 }
 
+export const addProducts = async (params: AddProductParams = {}) => {
+    const {
+        categoryId = 3,
+        productName = '',
+        productPrice = 0,
+        stock = 0,
+        productDescription = '',
+        productNameEn = '',
+        productDescriptionEn = '',
+        files = {},
+        lang = 'zh-TW'
+    } = params
+
+    const formData = new FormData();
+
+    formData.append('category_id', String(categoryId));
+    formData.append('product_name', productName);
+    formData.append('product_price', String(productPrice));
+    formData.append('stock', String(stock));
+    formData.append('product_description', productDescription);
+    formData.append('product_name_en', productNameEn);
+    formData.append('product_description_en', productDescriptionEn);
+
+    // 圖片（多張）
+    if (files.productImg)
+        formData.append('productImg', files.productImg);
+
+    if (files.photoContentMain)
+        formData.append('photoContentMain', files.photoContentMain);
+
+    if (files.photoContentSecondary)
+        formData.append('photoContentSecondary', files.photoContentSecondary);
+
+    if (files.photoContent)
+        formData.append('photoContent', files.photoContent);
+
+
+    const response = await fetch(
+        PRODUCTS_ADD,
+        {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Accept-Language': lang,
+            }
+        }
+    )
+
+    const data = await response.json()
+    return data
+}
